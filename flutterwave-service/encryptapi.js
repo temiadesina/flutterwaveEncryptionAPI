149e87609045ce1dac815b46b8db4ca435712b1a
temi adesina <adesinatemiloluwa@gmail.com>
2017-01-31 21:02:33 +0100

    removed some logged data from code

--- flutterwave-service/encryptapi.js
+++ flutterwave-service/encryptapi.js
@@ -6,19 +6,14 @@ module.exports = {
     var returnObject = {};
 
     var payload = req.body.jsonbody;
-    console.log(payload);
     var encrypt_key = req.body.encryption_key;
-    console.log(encrypt_key);
     for (var prop in payload){
       if (payload.hasOwnProperty(prop)) {
-        console.log(payload[prop]);
         var value = payload[prop];
         returnObject[prop] = encrypt(encrypt_key, value)
       }
 
     }
-
-    console.log(returnObject);
 
      return res.json(returnObject);
     //console.log(returnObject);
