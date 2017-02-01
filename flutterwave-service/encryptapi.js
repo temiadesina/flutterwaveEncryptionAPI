aefef5bd19512c65c6302bb171676491a7a59f67
temi adesina <adesinatemiloluwa@gmail.com>
2017-02-01 10:20:05 +0100

    added an object property merchantid

--- flutterwave-service/encryptapi.js
+++ flutterwave-service/encryptapi.js
@@ -7,14 +7,15 @@ module.exports = {
 
     var payload = req.body.jsonbody;
     var encrypt_key = req.body.encryption_key;
+    var merchant_key = req.body.merchantid;
     for (var prop in payload){
       if (payload.hasOwnProperty(prop)) {
         var value = payload[prop];
+
         returnObject[prop] = encrypt(encrypt_key, value)
       }
-
     }
-
+    returnObject.merchantid = merchant_key;
      return res.json(returnObject);
     //console.log(returnObject);
   }
